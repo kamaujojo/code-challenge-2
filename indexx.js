621c92dfe31024efff6b66f4fc43e47c49425744
joan <joan.kamau@student.moringaschool.com>
2024-07-08 23:32:00 +0300

    initial commit

--- indexx.js
+++
@@ -1,8 +0,0 @@
-function addItem(grapes) {
-    const itemInput = document.getElementById('grapes');
-    const itemName = itemInput.value.
-    if (grapes === '') return; 
-    
-    const itemList = document.getElementById('itemList');}
-
-  
